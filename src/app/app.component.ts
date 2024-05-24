7dfa0d44888964e5757c949ab980836c2f3635f3
André Mello <andreammellojr@gmail.com>
2024-05-24 16:36:25 -0300

    changed styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DarkModeService } from './dark-mode.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit, OnDestroy {
+  isDarkMode: boolean = false;
+  darkModeSubscription: Subscription;
+
+  constructor(
+    private darkModeService: DarkModeService,
+  ) { }
+
+  ngOnInit() {
+    this.darkModeSubscription = this.darkModeService.darkMode$.subscribe(darkMode => {
+      this.isDarkMode = darkMode;
+
+      if (darkMode) {
+        document.documentElement.removeAttribute('data-theme');
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+      }
+    });
+  }
+
+  toggleDarkMode(): void {
+    this.darkModeService.toggleDarkMode();
+  }
+
+  ngOnDestroy() {
+    if (this.darkModeSubscription) {
+      this.darkModeSubscription.unsubscribe();
+    }
+  }
+
 }
